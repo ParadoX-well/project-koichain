452e6c702987d6a925817008b28cde6171c6292d
ParadoX-well <dikaattarfizrah@gmail.com>
2026-06-17 20:45:04 +0700

    update 13 after hosting (update besar)

--- webta/lib/contractConfig.ts
+++ webta/lib/contractConfig.ts
@@ -1,4 +1,4 @@
-import KoiCertArtifact from '../artifacts/contracts/KoiCert.sol/KoiCert.json';
+import KoiCertArtifact from './abi/KoiCert.json';
 
 // ⚠️ GANTI ALAMAT INI DENGAN ALAMAT KONTRAK TERBARU SETIAP KALI DEPLOY ULANG
 export const CONTRACT_ADDRESS = "0xEF8e3e9899101a1fdb05e535a25582165d34185E";
